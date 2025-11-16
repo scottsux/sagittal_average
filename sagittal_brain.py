d5153fb021c071f9df31f7fe3623fb2bdba937c0
xinrui su <scottsu13@outlook.com>
2025-11-16 13:23:56 +0000

    modify

--- sagittal_brain.py
+++ sagittal_brain.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv'):
+
+def run_averages(file_input="brain_sample.csv", file_output="brain_average.csv"):
     """
     Calculates the average through the coronal planes
     The input file should has as many columns as coronal planes
@@ -9,11 +10,11 @@ def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv')
     The result is an average for each sagittal/horizontal plane (rows)
     """
     # Open the file to analyse
-    with open(file_input, 'r') as myfile:
-            # Create a plane list to keep a list per row
-            planes = []
-            for line in myfile.readlines():
-                planes.append([int(x) for x in line.split("\n")[0].split(',')])
+    with open(file_input, "r") as myfile:
+        # Create a plane list to keep a list per row
+        planes = []
+        for line in myfile.readlines():
+            planes.append([int(x) for x in line.split("\n")[0].split(",")])
 
     # let's use NumPy! It's faster!!
     planes = np.array(planes)
@@ -22,22 +23,34 @@ def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv')
     averages = [str(x) for x in averages]
 
     # write it out on my file
-    with open(file_output, 'w') as myoutput:
-             myoutput.write(','.join(averages) +  '\n')
+    with open(file_output, "w") as myoutput:
+        myoutput.write(",".join(averages) + "\n")
+
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
-    parser = ArgumentParser(description = "Calculates the average for each sagittal-horizontal plane.")
-    parser.add_argument('file_input', nargs='?', help="Input CSV file with the results from scikit-brain binning algorithm. If not provided it looks at brain_sample.csv")
-    parser.add_argument('--file_output', '-o', help="Name of the output CSV file. Defaults to brain_average.csv")
+
+    parser = ArgumentParser(
+        description="Calculates the average for each sagittal-horizontal plane."
+    )
+    parser.add_argument(
+        "file_input",
+        nargs="?",
+        help="Input CSV file with the results from scikit-brain binning algorithm. If not provided it looks at brain_sample.csv",
+    )
+    parser.add_argument(
+        "--file_output",
+        "-o",
+        help="Name of the output CSV file. Defaults to brain_average.csv",
+    )
     arguments = parser.parse_args()
 
     if arguments.file_input and arguments.file_output:
-         run_averages(arguments.file_input, arguments.file_output)
+        run_averages(arguments.file_input, arguments.file_output)
     elif arguments.file_input or arguments.file_output:
-         if arguments.file_input:
-             run_averages(arguments.file_input)
-         else:
-             run_averages(file_output=arguments.file_output)
+        if arguments.file_input:
+            run_averages(arguments.file_input)
+        else:
+            run_averages(file_output=arguments.file_output)
     else:
-         run_averages()
+        run_averages()
